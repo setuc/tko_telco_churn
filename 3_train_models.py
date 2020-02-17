e1d4a04577c15f61a8d43f696e39f3d1b3910805
Christopher Royles <christopher@royles.net>
2020-02-17 11:50:40 +0000

    Update 3_train_models.py

--- 3_train_models.py
+++ 3_train_models.py
@@ -1,6 +1,19 @@
-# This code will build a new model into the ~/models directory
-# To use this new model, change and save the CHURN_MODEL_NAME environment 
-# variable
+#This code will train a model
+
+
 
 from churnexplainer import train
-train.train_and_explain_and_save()
+from churnexplainer.data import dataset, load_dataset
+import cdsw, os
+
+#os.getenv('CHURN_MODEL_TYPE', 'linear') #| gb | nonlinear | voting"
+os.getenv('CHURN_MODEL_TYPE', sys.argv[1])
+#os.getenv('CHURN_DATASET', 'ibm') #| breastcancer | iris | telco
+os.getenv('CHURN_DATASET', sys.argv[2])
+
+train_score, test_score, model_path = train.experiment_and_save()
+
+cdsw.track_metric("train_score",round(train_score,2))
+cdsw.track_metric("test_score",round(test_score,2))
+cdsw.track_metric("model_path",model_path)
+cdsw.track_file(model_path)
